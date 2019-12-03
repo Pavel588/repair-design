bee37c02db98887c0364ab879304e0988259989f
Pavel Zhukov <ecko5849@gmail.com>
2019-12-03 19:07:30 +0300

    fix: gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,29 @@ const gulp = require("gulp")
 const browserSync = require('browser-sync').create();
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
- 
-gulp.task("hello", function(done) {
-    console.log("Привет, мир!");
-    done();
-});
 
+
+ 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function (done) { 
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
+
+    gulp.watch("./css/*.css", cssmin); 
 });
 
-gulp.task('default', function (done) {
-    gulp.src('./css/*.css')
+
+
+gulp.task('cssmin', function (done) {
+    gulp.src(['./css/*.css', '!./css/*.min.css']) 
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest("./css"));
         done();
 });
+
+gulp.task('build', gulp.series('cssmin', 'browser-sync'));
