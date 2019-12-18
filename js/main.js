171e3566040e4acc365811dfb3f41b004bca2bc0
Pavel Zhukov <ecko5849@gmail.com>
2019-12-19 01:12:15 +0300

    choe section project

--- js/main.js
+++ js/main.js
@@ -26,7 +26,21 @@ $(document).ready(function () {
   });
   
   var mySwiper = new Swiper ('.swiper-container', {
-    loop: true
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   })
 
+  var next = $('.swiper-button-next')
+  var prev = $('.swiper-button-prev')
+  var bullets = $('.swiper-pagination')
+
+  next.css('left', prev.width() + 10 + bullets.width())
+  bullets.css('left', prev.width() + 10)
 });
